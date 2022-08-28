13696bfa0a784a588b68cc271ed79f6f4cf91dcc
yoonjuho <dbswn990@naver.com>
2022-08-28 19:32:08 +0900

    docs: 01)Ready

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import Library from './chapter_03/Library'; //우리가 만든거 랜더링 작업
+import Clock from './chapter_04/Clock';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); //ReactDom을 사용하여 rootDom에 랜더링 하는 작업
+setInterval(() => {root.render(
   <React.StrictMode>
-    <App />
+    <Clock />
   </React.StrictMode>
-);
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

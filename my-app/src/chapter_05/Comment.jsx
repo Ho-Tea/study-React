d6103ab61b0cedd1ef84e86d73db10552a6884e5
yoonjuho <dbswn990@naver.com>
2022-08-29 23:42:52 +0900

    feat: my-app

---
+++ my-app/src/chapter_05/Comment.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+// css스타일 작성
+const styles = {
+  wrapper: {
+    margin: 8,
+    padding: 8,
+    display: "flex",
+    flexDirection: "row",
+    border: "1px solid grey",
+    borderRadius: 16,
+  },
+  imageContainer: {},
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  contentContainer: {
+    marginLeft: 8,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  nameText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  commentText: {
+    color: "black",
+    fontSize: 16,
+  },
+};
+
+
+
+
+function Comment(props){
+  return (
+    <div>
+        <h1>제가 만든 첫 컴포넌트 입니다</h1>
+    </div>
+  );
+}
+
+export default Comment;

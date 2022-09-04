8c45fe6b9923ebc76d26fe697c432d9766d61b82
yoonjuho <dbswn990@naver.com>
2022-09-04 23:36:22 +0900

    feat: Composition vs Inheritance

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,11 +9,16 @@ import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //ReactDom을 사용하여 rootDom에 랜더링 하는 작업
 root.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <ProfileCard />
   </React.StrictMode>
   );
 

2ebaa1053c6fb509e2ac742873a430ea7166fa9d
yoonjuho <dbswn990@naver.com>
2022-09-05 23:39:42 +0900

    feat: 15)Context

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -14,11 +14,12 @@ import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
 import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //ReactDom을 사용하여 rootDom에 랜더링 하는 작업
 root.render(
   <React.StrictMode>
-    <ProfileCard />
+    <DarkOrLight />
   </React.StrictMode>
   );
 

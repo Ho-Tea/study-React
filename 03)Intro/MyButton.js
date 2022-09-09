e2eba08e7dfe24f41efa805b67905404a510fbdf
yoonjuho <dbswn990@naver.com>
2022-09-10 01:29:42 +0900

    docs: 03)Intro

--- 03)Intro/MyButton.js
+++ 03)Intro/MyButton.js
@@ -1,7 +1,7 @@
 // 리액트 컴포넌트
 
 function MyButton(props){
-  const [isClicked, setIsClicked] = React.useState(false)
+  const [isClicked, setIsClicked] = React.useState(false) //react-dom.development.js 파일에서 전역변수로 생성된다
 
   return React.createElement(
       'button',
@@ -10,5 +10,5 @@ function MyButton(props){
   )
 }
 
-const domContainer = document.querySelector('#root'); //dom이랑 랜더링하는 것
-ReactDOM.render(React.createElement(MyButton), domContainer)
+const domContainer = document.querySelector('#root'); //미리 만들어 놓은 index.html에 존재하는 돔 요소를 가져온다 
+ReactDOM.render(React.createElement(MyButton), domContainer)  //react-dom.development.js 파일에서 전역변수로 만든 ReactDOM변수를 사용해서 우리가 만든 컴포넌트를 root돔 요소에 붙인다
